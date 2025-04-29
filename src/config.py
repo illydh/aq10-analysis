e57fa93eac2d4c42480cb807b4277cae03d715d8
Illy Hoang <133312266+illydh@users.noreply.github.com>
2025-04-28 21:32:35 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,15 +1,29 @@
 from pathlib import Path
+import torch
 
 
 class Config:
+    """
+    Configuration class for the project.
+    """
+
+    SEED = 42
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
     # Data configuration
-    PROJECT_PATH = Path("path/to/project")
-    RAW_DATA_PATH = PROJECT_PATH / Path("data/raw/autism_screening.csv")
-    PROCESSED_DATA_DIR = PROJECT_PATH / Path("data/processed")
+    PROJECT_PATH = Path("/content/drive/MyDrive/Projects/Spring-2025-Research")
+
+    #   source: https://www.kaggle.com/datasets/andrewmvd/autism-screening-on-adults
+    RAW_ADULT_DATA_PATH = PROJECT_PATH / Path("dataset/autism_screening.csv")
+    #   source: https://www.kaggle.com/datasets/uppulurimadhuri/dataset/data
+    RAW_CHILD_DATA_PATH = PROJECT_PATH / Path("dataset/data_csv.csv")
+
+    PROCESSED_DATA_DIR = PROJECT_PATH / Path("dataset/processed")
     CHECKPOINT_DIR = PROJECT_PATH / Path("checkpoints")
     MODEL_DIR = PROJECT_PATH / Path("models")
 
     # Model hyperparameters
+    MODEL_NAME = "bert-base-uncased"
     MODEL_DIM = 64
     NUM_LAYERS = 4
     DROPOUT = 0.2
@@ -18,10 +32,14 @@ class Config:
     EPOCHS = 20
 
     # Data processing
-    FEATURE_LEN = 13  # AQ10 scores + age + gender + result
+    FEATURE_LEN = 12  # AQ10 scores + age + gender
 
     @classmethod
     def setup_directories(cls):
+        """
+        Setup directories for storing processed data, model checkpoints, and model files.
+        """
+
         cls.PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
         cls.CHECKPOINT_DIR.mkdir(parents=True, exist_ok=True)
         cls.MODEL_DIR.mkdir(parents=True, exist_ok=True)
